0e383cfa1e3ba346eb6fe8f252d12b26280897a7
Fernando Carvalho <fernanduzz@gmail.com>
2018-07-30 16:03:01 -0300

    normalization of data

--- ia.py
+++ ia.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn import preprocessing
 
 LABEL_COL = 20
-MALE = [1,0]
-FEMALE = [0,1]
-
+MALE = [1, 0]
+FEMALE = [0, 1]
 
 def get_data():
     data = np.array(pd.read_csv('voice.csv'))
+    shuffle(data)
     labels = data[:, [LABEL_COL]]
     data = np.delete(data, LABEL_COL, axis=1)
 
@@ -21,7 +22,7 @@ def get_data():
             y.append(MALE)
         else:
             y.append(FEMALE)
-	
+
     return data, np.array(y)
 
 
@@ -29,7 +30,8 @@ def keras_model():
 
     model = tf.keras.Sequential()
 
-    model.add(tf.keras.layers.Dense(32, activation=tf.nn.elu, input_shape=(20,)))
+    model.add(tf.keras.layers.Dense(
+        32, activation=tf.nn.elu, input_shape=(20,)))
     model.add(tf.keras.layers.Dense(64, activation=tf.nn.elu))
     model.add(tf.keras.layers.Dense(128, activation=tf.nn.elu))
     model.add(tf.keras.layers.Dense(64, activation=tf.nn.elu))
@@ -39,22 +41,35 @@ def keras_model():
     return model
 
 
-#model = keras_model()
-model = tf.keras.models.load_model('ia.model')#= keras_model()
+model = keras_model()
+#model = tf.keras.models.load_model('ia.model')
 
 model.compile(
     loss=tf.keras.losses.mean_squared_error,
     optimizer=tf.keras.optimizers.SGD(1e-2),
-    metrics=['accuracy']
+    metrics=['accuracy'],
 )
+
 features, labels = get_data()
+features = preprocessing.MinMaxScaler().fit_transform(features)
+
+train_features = features[:-500]
+test_features = features[-500:]
+train_labels = labels[:-500]
+test_labels = labels[-500:]
+
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir='./logs/tensorboard', histogram_freq=0,
+                                             write_graph=True, write_images=False)
+
 model.fit(
     features,
     labels,
-    epochs=1000,
+    epochs=500,
     shuffle=True,
-    batch_size=5
-
+    batch_size=5,
+    callbacks=[tensorboard],
+    validation_split=0.2
 )
 
-model.save('ia.model')
+#model.save('ia.model')
